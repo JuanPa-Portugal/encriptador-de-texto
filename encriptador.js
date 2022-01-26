441a1eb4801e15d4a3eea33851a85c9be74524ab
juanPaPortugal <pabloayoroa23@gmail.com>
2022-01-26 11:20:25 -0300

    finalizando funciones para encriptar y desencriptar textos y boton copiar

--- encriptador.js
+++ encriptador.js
@@ -1,34 +1,14 @@
-function encriptacion(texto){
-    var esVocal = false;
-    var encriptado = "";
+function encriptacion(texto) {
 
-    for (let i = 0; i < texto.length; i++) {
-        if (texto[i] == "a") {
-          encriptado = encriptado + texto[i].replace("a", "ai");
-          esVocal = true;
-        }
-        if (texto[i] == "e") {
-          encriptado = encriptado + texto[i].replace("e", "enter");
-          esVocal = true;
-        }
-        if (texto[i] == "i") {
-          encriptado = encriptado + texto[i].replace("i", "imes");
-          esVocal = true;
-        }
-        if (texto[i] == "o") {
-          encriptado = encriptado + texto[i].replace("o", "ober");
-          esVocal = true;
-        }
-        if (texto[i] == "u") {
-          encriptado = encriptado + texto[i].replace("u", "ufat");
-          esVocal = true;
-        }
-        if (esVocal == false) {
-          encriptado = encriptado + texto[i];
-        } else {
-          esVocal = false;
-        }
-      }
-      return encriptado;
-      
-  }
+  var encriptado = texto.replace(/a/gi, 'ai').replace(/e/gi, 'enter').replace(/i/gi, 'imes').replace(/o/gi, 'ober').replace(/u/gi, 'ufat');
+ 
+  return encriptado;
+}
+
+function desencriptacion(texto){
+  var desencriptado = texto.replace(/ai/gi, "a").replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+  return desencriptado;
+}
+
+
+
--- script.js
+++ script.js
@@ -1,36 +1,41 @@
-/* Reglas de encriptación: 
-"e" es convertido para "enter" 
-"i" es convertido para "imes"
-"a" es convertido para "ai"
-"o" es convertido para "ober"
-"u" es convertido para "ufat"
-Solo letras minusculas
-No se permite acentuación de palabras 
-*/
 
-/* Reglas de desencriptación: 
-"enter" es convertido para "e" 
-"imes" es convertido para "i"
-"ai" es convertido para "a"
-"ober" es convertido para "o"
-"ufat" es convertido para "u"
-Solo letras minusculas
-No se permite acentuación de palabras   
-*/
+//encriptando texto
+var btnEncriptar = document.querySelector("#btn-encriptar"); //boton para encriptar
 
-var codigosEncriptados = ["ai", "enter", "imes", "ober", "ufat"];
-var vocales = ["a", "e", "i", "o", "u"];
-
-var btnEncriptar = document.querySelector("#btn-encriptar"); //
+//capturando texto para encriptar con el botton encriptar
 btnEncriptar.addEventListener("click", function (event) {
   event.preventDefault();
-  //capturando texto para encriptar
   var texto = document.querySelector("#input-texto").value; //
   var msgEncriptado = document.querySelector("#msg");
-
+  //llamando a la funcion encriptacion para encriptar texto
   var codificado = encriptacion(texto);
-  msgEncriptado.value = codificado;
-  
+  msgEncriptado.value = codificado;// deveulve texto encriptado en la caja de resultado
 });
+
+
+//desencriptando texto
+
+ var btnDesencriptar= document.querySelector("#btn-desencriptar");
+
+ //capturando texto para desencriptar con el botton desencriptar
+    btnDesencriptar.addEventListener("click",function(e){
+        e.preventDefault();
+
+        var title=document.querySelector("#title");
+        title.innerHTML="Mensaje desencriptado";// cambia el titulo al hacer click en desencritar 
+        var textoParaDecodificar = document.querySelector("#input-texto").value;
+        //llamando a la funcion encriptacion en su funcion desencriptar texto
+        var decodificado= desencriptacion(textoParaDecodificar);
+        var desencriptado = document.querySelector("#msg");
+        desencriptado.value = decodificado;//devuelve texto desencriptado en la caja de resultado
+    }) 
+// boton para copiar texto encriptado en el portapapeles
+var btnCopiar = document.querySelector("#btn-copy");
+btnCopiar.addEventListener("click",function(){
+    let texto = document.querySelector("#msg");
+    texto.select();
+    document.execCommand("copy");
+})
+
 
  
